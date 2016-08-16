ee951aec65222be0733d8da1fa6d6394d30f0755
Manuel Olguin <molguin@dcc.uchile.cl>
2016-08-16 00:13:57 -0300

    JFrame

--- src/main/java/org/olguin/moulinette/MoulinetteApplication.java
+++ src/main/java/org/olguin/moulinette/MoulinetteApplication.java
@@ -1,6 +1,12 @@
 package org.olguin.moulinette;
 
 import com.github.kevinsawicki.http.HttpRequest;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Manuel Olguín (molguin@dcc.uchile.cl) on 2016-08-15.
@@ -8,9 +14,39 @@ import com.github.kevinsawicki.http.HttpRequest;
  */
 public class MoulinetteApplication {
 
+    private MoulinetteApplication(int width, int height)
+    {
+        JFrame window = new JFrame();
+        window.setSize(width, height);
+
+        JButton b=new JButton("click");//creating instance of JButton
+        b.setBounds(width/2 - 50,height/2 - 20,100, 40);//x axis, y axis, width, height
+        b.addActionListener(new ButtonActionListener());
+
+        window.add(b);//adding button in JFrame
+
+        window.setLayout(null);
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
     public static void main(String [] args)
     {
-        String response = HttpRequest.get("https://moulinetteweb.herokuapp.com/api/v1/homeworks").body();
-        System.out.println("Response was: " + response);
+        new MoulinetteApplication(800, 600);
+    }
+
+    private class ButtonActionListener implements ActionListener
+    {
+
+        public void actionPerformed(ActionEvent e) {
+            String response = HttpRequest.get("https://moulinetteweb.herokuapp.com/api/v1/homeworks").body();
+            JSONObject obj = new JSONObject(response);
+            int len = obj.getInt("len");
+            JSONArray arr = obj.getJSONArray("result");
+
+
+            System.out.println(len);
+            System.out.println(arr);
+        }
     }
 }
