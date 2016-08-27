5cdf1a394ec976b2156c3129e1f89ef9d6da514c
Manuel Olguin <molguin@dcc.uchile.cl>
2016-08-27 16:54:15 -0300

    Added debugging option, to connect locally or to heroku.

--- src/main/java/org/olguin/moulinette/MoulinetteServerManager.java
+++ src/main/java/org/olguin/moulinette/MoulinetteServerManager.java
@@ -14,16 +14,29 @@ import java.util.List;
  * Created by Manuel Olguín (molguin@dcc.uchile.cl) on 2016-08-21.
  * Part of org.olguin.moulinette.
  */
-public class MoulinetteServerManager {
+public class MoulinetteServerManager
+{
 
-    private String serveruri = "https://moulinetteweb.herokuapp.com/api/v1/";
+    private String serveruri;
     private List<Homework> homeworks;
 
-    public void updateHomeworks() {
+    public MoulinetteServerManager()
+    {
+
+        if (System.getenv("MOULINETTE_DEBUG").equals("TRUE"))
+            serveruri = System.getenv("SERVER_URL") + "/api/v1/";
+        else
+            serveruri = "https://moulinetteweb.herokuapp.com/api/v1/";
+    }
+
+
+    public void updateHomeworks()
+    {
         homeworks = new ArrayList<>(10);
         String res = HttpRequest.get(serveruri + "homeworks").body();
         JSONArray hws = new JSONObject(res).getJSONArray("result");
-        for (Object o : hws) {
+        for (Object o : hws)
+        {
             JSONObject hw = (JSONObject) o;
             String id = hw.getString("id");
             String name = hw.getString("name");
@@ -31,7 +44,8 @@ public class MoulinetteServerManager {
             List<HomeworkItem> items = new ArrayList<>(3);
             JSONArray jitems = hw.getJSONArray("items");
 
-            for (Object o1 : jitems) {
+            for (Object o1 : jitems)
+            {
                 JSONObject it = (JSONObject) o1;
                 HomeworkItem item = new HomeworkItem(
                         it.getString("id"),
@@ -40,7 +54,8 @@ public class MoulinetteServerManager {
                 );
 
                 JSONArray jtests = it.getJSONArray("tests");
-                for (Object o2 : jtests) {
+                for (Object o2 : jtests)
+                {
                     JSONObject test = (JSONObject) o2;
                     item.addTest(test.getString("id"), test.getString("description"), test.getString("input"));
                 }
@@ -53,7 +68,8 @@ public class MoulinetteServerManager {
         }
     }
 
-    public void validateTestOutput(String testid, String output) throws WrongResult {
+    public void validateTestOutput(String testid, String output) throws WrongResult
+    {
         HttpRequest res = HttpRequest.post(serveruri + "validate_test", true, "id", testid, "output", output + "\n");
 
         if (res.notFound() || res.badRequest())
@@ -64,14 +80,17 @@ public class MoulinetteServerManager {
             throw new WrongResult(result.getString("error"));
     }
 
-    public List<Homework> getHomeworks() {
+    public List<Homework> getHomeworks()
+    {
         return this.homeworks;
     }
 
-    public class WrongResult extends Exception {
+    public class WrongResult extends Exception
+    {
         public String error;
 
-        public WrongResult(String error) {
+        public WrongResult(String error)
+        {
             this.error = error;
         }
     }
